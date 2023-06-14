3efd1b26bd9dd8f0642b74c31e09aea9bc4be02b
Saiteja <msssteja25@gmail.com>
2023-06-14 21:23:32 +0530

    day2 programs added

---
+++ Module 1/Day 2/solution 1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+void printExponent(double x) {
+    unsigned long long *ptr = (unsigned long long *)&x;
+    unsigned long long exponent = (*ptr & 0x7FF0000000000000ULL) >> 52;
+
+    // Hexadecimal format
+    printf("Hexadecimal: 0x%llX\n", exponent);
+
+    // Binary format
+    printf("Binary: 0b");
+    int i;
+    for (i = 11; i >= 0; i--) {
+        if (exponent & (1ULL << i))
+            printf("1");
+        else
+            printf("0");
+    }
+    printf("\n");
+}
+
+int main() {
+    double x = 0.7;
+    printExponent(x);
+
+    return 0;
+}
